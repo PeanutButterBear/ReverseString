c58290cccfbff812a5570701833124f48cc3026d
Calvin Gates <100246326+PeanutButterBear@users.noreply.github.com>
2023-09-30 13:08:09 -0400

    Initial commit for alternative-method branch
    Adding alternative method to reverse string

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,26 +1,33 @@
 package com.example;
 
 public class Main {
-
+    //alternative method of reversing strings I found in a youtube video https://www.youtube.com/watch?v=orUTq3CahRE&list=PL59LTecnGM1MMSBPiLN4aMwM7DcnVld0s&index=1
     public static void main(String[] args) {
-        System.out.println(reverse("dog"));
+        String r = reverse("dog");
+        System.out.println(r);
     }
 
     //public means it can be accessed by other class
     //static means this method can be called without creating an instance of the (Main) class
     public static String reverse(String s) {
-        //initializing two arrays: one to store the original string as character array, one empty character array to hold reversed original string
+        //initializing letters array to store the letters of original string as character array
         //initializing variable to represent first element of reversed array
-        char[] reversed = new char[s.length()];
-        char[] originalString = s.toCharArray();
-        int p = 0;
-        //loop to iterate through original array backwards, the last element of originalString will be assigned to first element of reversed
-        for (int i = s.length() - 1; i >= 0; i--) {
-            reversed[p] = originalString[i];
-            p++;
+        char[] letters = new char[s.length()];
+        int letterIndex = 0;
+
+        //iterating backwards through original array to get last character first,
+        //then assigning that last character of ogarray to the first letter of letters array
+        for(int i = s.length() - 1; i >= 0; i--) {
+            letters[letterIndex] = s.charAt(i);
+            letterIndex++;
         }
-        //This method must return a string so creating new string with value of the reversed character array
-        String str = new String(reversed);
-        return str;
+
+        //initializing empty string, then appending each character of our letters array which has the reverse string
+        //so that we can return a string
+        String reverse = "";
+        for(int i = 0; i < s.length(); i++) {
+            reverse = reverse + letters[i];
+        }
+        return reverse;
     }
 }
